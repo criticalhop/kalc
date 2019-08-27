dd5750bf1ce2a04fd76c06b92bfc9b9dc8a826ba
makarty <vasily@amanda.us-central1-c.c.arched-canyon-836.internal>
2019-08-27 21:20:20 +0000

    status list

--- guardctl/misc/status.py
+++ guardctl/misc/status.py
@@ -4,112 +4,83 @@ from guradctl.model.action.oom_kill import *
 from guradctl.model.action.scheduler import *
 import poodle.problem
 
-class ProblemTemplate(poodle.problem.Problem):
-    constSymbol = {}
-    pod = []
-    node = []
-    kubeProxy = []
-    loadbalancer = []
-    service = []
-    daemonSet = []
-    request = []
-    containerConfig = []
-    priorityDict = {}
+        STATUSREQATSTART = Status()
+        STATUSREQATLOADBALANCER = Status()
+        STATUSREQATKUBEPROXY = Status()
+        STATUSREQATPODINPUT = Status()
+        STATUSREQMEMRESOURCECONSUMED = Status()
+        STATUSREQCPURESOURCECONSUMED = Status()
+        STATUSREQRESOURCESCONSUMED = Status()
+        STATUSREQDIRECTEDTOPOD = Status()
+        STATUSREQREQUESTPIDTOBEENDED = Status()
+        STATUSREQCPURESOURCERELEASED = Status()
+        STATUSREQMEMRESOURCERELEASED = Status()
+        STATUSREQRESOURCESRELEASED = Status()
+        STATUSREQREQUESTTERMINATED = Status()
+        STATUSREQREQUESTFINISHED = Status()
+        STATUSPODATCONFIG = Status()
+        STATUSPODREADYTOSTART = Status()
+        STATUSPODACTIVE = Status()
+        STATUSPODPENDING = Status()
+        STATUSPODATMANUALCREATION = Status()
+        STATUSPODDIRECTEDTONODE = Status()
+        STATUSPODCPUCONSUMED = Status()
+        STATUSPODRESOURCEFORMALCONSUMPTIONFAILED = Status()
+        STATUSPODFAILEDTOSTART = Status()
+        STATUSPODREADYTOSTART2 = Status()
+        STATUSPODMEMCONSUMED = Status()
+        STATUSPODMEMCONSUMEDFAILED = Status()
+        STATUSPODBINDEDTONODE = Status()
+        STATUSPODRUNNING = Status()
+        STATUSPODSUCCEEDED = Status() # MAY BE LOST BE CAREFUL
+        STATUSPODKILLING = Status()
+        STATUSPODFAILED = Status()
+        STATUSNODERUNNING = Status()
+        STATUSNODESUCCEDED = Status()
+        STATUSPODPENDING = Status()
+        STATUSNODEDELETED = Status()
+        STATUSPODINACTIVE = Status()
+        STATUSNODEACTIVE = Status()
+        STATUSNODEINACTIVE = Status()
+        STATUSREQDIRECTEDTONODE = Status()
+        STATUSREQNODECAPACITYOVERWHELMED = Status()
+        STATUSLIMMET = Status()
+        STATUSLIMOVERWHELMED = Status()
+        TEST = Status()
+        STATUSPODTOBETERMINATED = Status()
+        STATUSPODTERMINATED = Status()
+        STATUSSERVPENDING = Status()
+        STATUSSERVSTARTED = Status()
+        STATUSSERVINTERRUPTED = Status()
+        STATUSREQRUNNING = Status()
+        STATUSPODINITIALCONRELEASED = Status()
+        STATUSPODGLOBALMEMCONSUMED = Status()
+        STATUSPODGLOBALCPUCONSUMED = Status()
+        STATUSPODFORMALCONRELEASED = Status()
+        STATUSSCHEDCLEAN = Status()
+        STATUSSCHEDCHANGED = Status()
+        STATUSPODREADYTOSTART = Status()
+        STATUSPODFINISHEDPLACEMENT = Status()
+        
+        STATEPODSUCCEEDED = State()
+        STATEPODRUNNING = State()
+        STATEPODPENDING = State()
+        STATEPODACTIVE = State()
+        STATEPODINACTIVE = State()
+        STATEREQSTARTED = State()
+        STATEREQUESTACTIVE = State()
+        STATEREQUESTINACTIVE = State()
+        STATENODEACTIVE = State()
+        STATENODEINACTIVE = State()
 
-    def constFactory(self, statusNameList, objType):
-        for statusName in statusNameList:
-            self.constSymbol[statusName] = self.addObject(objType(statusName))
-
-    def problem(self):
-        statusList = ["statusReqAtStart",
-                        "statusReqAtLoadbalancer",
-                        "statusReqAtKubeproxy",
-                        "statusReqAtPodInput",
-                        "statusReqMemResourceConsumed",
-                        "statusReqCpuResourceConsumed",
-                        "statusReqResourcesConsumed",
-                        "statusReqDirectedToPod",
-                        "statusReqRequestPIDToBeEnded",
-                        "statusReqCpuResourceReleased",
-                        "statusReqMemResourceReleased",
-                        "statusReqResourcesReleased",
-                        "statusReqRequestTerminated",
-                        "statusReqRequestFinished",
-                        "statusPodAtConfig",
-                        "statusPodReadyToStart",
-                        "statusPodActive",
-                        "statusPodPending",
-                        "statusPodAtManualCreation",
-                        "statusPodDirectedToNode",
-                        "statusPodCpuConsumed",
-                        "statusPodResourceFormalConsumptionFailed",
-                        "statusPodFailedToStart",
-                        "statusPodReadyToStart2",
-                        "statusPodMemConsumed",
-                        "statusPodMemConsumedFailed",
-                        "statusPodBindedToNode",
-                        "statusPodRunning",
-                        "statusPodSucceeded", # may be lost be careful
-                        "statusPodKilling",
-                        "statusPodFailed",
-                        "statusNodeRunning",
-                        "statusNodeSucceded",
-                        "statusPodPending",
-                        "statusNodeDeleted",
-                        "statusPodInactive",
-                        "statusNodeActive",
-                        "statusNodeInactive",
-                        "statusReqDirectedToNode",
-                        "statusReqNodeCapacityOverwhelmed",
-                        "statusLimMet",
-                        "statusLimOverwhelmed",
-                        "test",
-                        "statusPodToBeTerminated",
-                        "statusPodTerminated",
-                        "statusServPending",
-                        "statusServStarted",
-                        "statusServInterrupted",
-                        "statusReqRunning",
-                        "statusPodInitialConReleased",
-                        "statusPodGlobalMemConsumed",
-                        "statusPodGlobalCpuConsumed",
-                        "statusPodFormalConReleased",
-                        "statusSchedClean",
-                        "statusSchedChanged",
-                        "statusPodReadyToStart",
-                        "statusPodFinishedPlacement"]
-        self.constFactory(statusList, Status)
-
-        stateList = [
-        "statePodSucceeded",
-        "statePodRunning",
-        "statePodPending",
-        "statePodActive",
-        "statePodInactive",
-        "stateReqStarted",
-        "stateRequestActive",
-        "stateRequestInactive",
-        "stateNodeActive",
-        "stateNodeInactive"]
-        self.constFactory(stateList, State)
-
-        typeList = ["typeTemporary","typePersistent","Null","notNull","Issue01","PreemptLowerPriority","Never"]
-        self.constFactory(typeList, Type)
-
-        modeList = ["modeUsermode","modeIptables"]
-        self.constFactory(modeList, Mode)
-
-        self.constSymbol["statusReqAtStart"].sequence =  1
-        self.constSymbol["statusReqAtLoadbalancer"].sequence =  2
-        self.constSymbol["statusReqAtKubeproxy"].sequence =  3
-        self.constSymbol["statusReqDirectedToPod"].sequence =  4
-        self.constSymbol["statusReqAtPodInput"].sequence =  5
-        self.constSymbol["statusReqCpuResourceConsumed"].sequence =  6
-        self.constSymbol["statusReqMemResourceConsumed"].sequence =  7
-        self.constSymbol["statusReqResourcesConsumed"].sequence =  8
-        self.constSymbol["statusReqRequestPIDToBeEnded"].sequence =  9
-        self.constSymbol["statusReqCpuResourceReleased"].sequence =  10
-        self.constSymbol["statusReqMemResourceReleased"].sequence =  11
-        self.constSymbol["statusReqResourcesReleased"].sequence =  12
-        self.constSymbol["statusReqRequestTerminated"].sequence =  13
-        self.constSymbol["statusReqRequestFinished"].sequence =  20
+        
+        TYPETEMPORARY = Type()
+        TYPEPERSISTENT = Type()
+        NULL = Type()
+        NOTNULL = Type()
+        ISSUE01 = Type()
+        PREEMPTLOWERPRIORITY = Type()
+        NEVER = Type()
+        
+        MODEUSERMODE = Mode()
+        MODEIPTABLES = MOde()
