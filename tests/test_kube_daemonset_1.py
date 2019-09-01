0c0028b81653cf21ba17b90287e9e3c9a0330c5d
Arty Mak <amaximovich@gmail.com>
2019-09-01 05:08:15 +0000

    kubetest added

---
+++ tests/test_kube_daemonset_1.py
@@ -0,0 +1,23 @@
+import pytest
+from poodle import * 
+from  tests.problem.goals import *
+from guardctl.misc.const import *
+
+daemonYAMLPath = "./tests/kube-config/daemon-set-custom.yaml"
+serviceYAMLPath = "./tests/kube-config/guestbook-all-in-one.yaml"
+
+# @pytest.mark.skip(reason="no way of currently testing this")
+def test():
+    dumpList = [daemonYAMLPath, './examples/currentCloud/coreV1_api_list_node.yaml', './examples/currentCloud/coreV1_api_list_pod_for_all_namespaces.yaml',  './examples/currentCloud/coreV1_list_service_for_all_namespaces.yaml','./examples/currentCloud/shV1beta1_api_list_priority_class.yaml']
+    tests = [TestServiceInterrupted()]
+    for t in tests: t.run()
+    import yaml
+    for p in tests:
+        if not p.plan: 
+            print("Could not solve %s" % p.__class__.__name__)
+            continue
+        print("Created plan for %s:" % p.__class__.__name__)
+        i=0
+        for a in p.plan:
+            i=i+1
+            print(i,":",a.__class__.__name__,"\n",yaml.dump({k:v.value if v else f"NONE{v}" for (k,v) in a.kwargs.items()}, default_flow_style=False))
