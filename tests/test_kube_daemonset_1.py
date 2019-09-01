092168c10526b0065906e0528e89408feb61168d
Arty Mak <amaximovich@gmail.com>
2019-09-01 14:58:30 +0000

    wip

--- tests/test_kube_daemonset_1.py
+++ tests/test_kube_daemonset_1.py
@@ -3,21 +3,8 @@ from poodle import *
 from  tests.problem.goals import *
 from guardctl.misc.const import *
 
-daemonYAMLPath = "./tests/kube-config/daemon-set-custom.yaml"
-serviceYAMLPath = "./tests/kube-config/guestbook-all-in-one.yaml"
-
 # @pytest.mark.skip(reason="no way of currently testing this")
 def test():
-    dumpList = [daemonYAMLPath, './examples/currentCloud/coreV1_api_list_node.yaml', './examples/currentCloud/coreV1_api_list_pod_for_all_namespaces.yaml',  './examples/currentCloud/coreV1_list_service_for_all_namespaces.yaml','./examples/currentCloud/shV1beta1_api_list_priority_class.yaml']
     tests = [TestServiceInterrupted()]
-    for t in tests: t.run()
-    import yaml
-    for p in tests:
-        if not p.plan: 
-            print("Could not solve %s" % p.__class__.__name__)
-            print(p)
-            continue
-        if p.plan:
-            print("Created plan for %s:" % p.__class__.__name__)
-            print(p)
-            assert p.plan
+    print(p.plan)
+    assert p.plan
