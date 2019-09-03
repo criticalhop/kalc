a60014e5073c90c5d23f86d9806de38b517e20a1
Arty Mak <amaximovich@gmail.com>
2019-09-03 21:42:17 +0000

    wip

--- tests/problem/goals.py
+++ tests/problem/goals.py
@@ -6,6 +6,18 @@ from guardctl.model.search import *
 
 
 
+class Test_case_1(Problem2,K8SearchEviction ):
+    def goal(self):
+        self.pod[0].status_phase == STATUS_POD_RUNNING
+
+class Test_case_2(Problem2,K8SearchEviction ):
+    def goal(self):
+        self.pod[1].status_phase == STATUS_POD_PENDING
+
+class Test_case_3(Problem2,K8SearchEviction ):
+    def goal(self):
+        self.pod[0].status_phase == STATUS_POD_KILLING
+
 class TestServiceInterrupted(Problem2,K8SearchEviction ):
     def goal(self):
         self.service[0].status == STATUS_SERV_INTERRUPTED
