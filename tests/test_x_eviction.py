61fd9a3d265264831057736e391c6f3ca66e9a0d
Makarty <amaximovich@gmail.com>
2019-09-04 19:58:36 +0000

    node info

--- tests/test_x_eviction.py
+++ tests/test_x_eviction.py
@@ -17,9 +17,18 @@ class SingleGoalEvictionDetect(K8SearchEviction):
     def goal(self):
         # for ob in self.objectList:
         #     print(str(ob))
-        podlist = filter(lambda x: isinstance(x, Pod), self.objectList)
-        for poditem in podlist:
+        pod_loaded_list = filter(lambda x: isinstance(x, Pod), self.objectList)
+        for poditem in pod_loaded_list:
             print("pod:"+ str(poditem.metadata_name._get_value()) + " status_phase: " + str(poditem.status_phase) + " spec_nodeName: " + str(poditem.spec_nodeName._get_value()) + " cpuRequest: " + str(poditem.cpuRequest._get_value()) + " memRequest: " + str(poditem.memRequest._get_value()) + " cpuLimit: " + str(poditem.cpuLimit._get_value()) + " memLimit: " + str(poditem.memLimit._get_value()))
+        node_loaded_list = filter(lambda x: isinstance(x, Node), self.objectList)
+        for nodeitem in node_loaded_list:
+            print("node:"+ str(nodeitem.metadata_name._get_value()) + " cpuCapacity: " + str(nodeitem.cpuCapacity._get_value()) + " memCapacity: " + str(nodeitem.memCapacity._get_value()) + \
+            " currentFormalCpuConsumption: "  + str(nodeitem.currentFormalCpuConsumption._get_value()) + \
+            " currentFormalMemConsumption: " + str(nodeitem.currentFormalMemConsumption._get_value()) + \
+            " AmountOfPodsOverwhelmingMemLimits: " + str(nodeitem.AmountOfPodsOverwhelmingMemLimits._get_value()) + \
+            " podAmount: "  + str(nodeitem.podAmount._get_value()) + \
+            " isNull:"  + str(nodeitem.isNull._get_value()) + \
+            " status:"  + str(nodeitem.status._get_value()))
 
         evict_service = next(filter(lambda x: isinstance(x, Service) and \
             labelFactory.get("app", "redis-evict") in x.spec_selector._get_value(),
