1b6a758126c7996c5dd788ea812c64629bd45629
Arty Mak <36539458+makarty@users.noreply.github.com>
2019-09-21 00:19:44 +0300

    Update test_x_exclude.py

--- tests/test_x_exclude.py
+++ tests/test_x_exclude.py
@@ -32,7 +32,6 @@ def test_ignore_check_mUster():
         "-t", "150", "-e",  "Service:redis-master-evict,Service:redis-mUster","--ignore-nonexistent-exclusions"])
     assert result.exit_code == 0
 
-# @pytest.mark.skip(reason="exclude for testing purpose")
 def test_excluded_search():
     result = CliRunner().invoke(run, ["--from-dir", TEST_CLUSTER_FOLDER, "-f", TEST_DAEMONET, "-o", "yaml", \
         "-t", "150", "-e",  "Service:redis-master-evict,Service:redis-master", "--pipe"])
