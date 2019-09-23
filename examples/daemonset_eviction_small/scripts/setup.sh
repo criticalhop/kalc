eabc14b66207877eb1e3944e7c17c8e24c472a6c
Makarty <amaximovich@gmail.com>
2019-09-23 21:29:00 +0000

    added example for #86

--- examples/daemonset_eviction_small/scripts/setup.sh
+++ examples/daemonset_eviction_small/scripts/setup.sh
@@ -1,3 +1,3 @@
-kubectl config view
+kubectl config view | grep "cluster:"
 kubectl config use-context gke_closercriticalhop_us-central1-a_testspace
 kubectl config set-context --current --namespace=default
