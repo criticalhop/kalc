7e9e5358faa54b3ca68fae6ada0563ee0e4bff74
Kuznetsov Andrey <andreykyz@gmail.com>
2019-09-26 21:40:53 +0300

    apply support in Deployment.py

--- guardctl/model/kinds/Deployment.py
+++ guardctl/model/kinds/Deployment.py
@@ -72,7 +72,7 @@ class Deployment(Controller, HasLimitsRequests):
         scheduler = next(filter(lambda x: isinstance(x, Scheduler), object_space))
         deployments = filter(lambda x: isinstance(x, Deployment), object_space)
         for deploymentController in deployments:
-            if str(deploymentController.metadata_name) == str(self.metadata_name):
+            if deploymentController != self and str(deploymentController.metadata_name) == str(self.metadata_name):
                 message = "Error from server (AlreadyExists): deployments.{0} \"{1}\" already exists".format(str(self.apiVersion).split("/")[0], self.metadata_name)
                 logger.error(message)
                 raise AssertionError(message)
@@ -100,6 +100,22 @@ class Deployment(Controller, HasLimitsRequests):
     def hook_scale_before_create(self, object_space, new_replicas):
         self.spec_replicas = new_replicas
 
+    def hook_after_apply(self, object_space):
+        deployments = filter(lambda x: isinstance(x, Deployment), object_space)
+        old_deployment = None
+        for deploymentController in deployments:
+            if deploymentController != self and str(deploymentController.metadata_name) == str(self.metadata_name):
+                old_deployment = deploymentController
+                break
+        # if old DEployment not found
+        if old_deployment == None:
+            self.hook_after_create(object_space)
+        else:
+            self.podList = old_deployment.podList # copy pods
+            self.hook_scale_after_load(object_space, self.spec_replicas._get_value()) # extend or trimm pods
+            object_space.remove(old_deployment) # delete old Deployment
+
+
     #Call me only atfter loading this Controller
     def hook_scale_after_load(self, object_space, new_replicas):
         diff_replicas = new_replicas - self.spec_replicas
@@ -107,11 +123,11 @@ class Deployment(Controller, HasLimitsRequests):
             logger.warning("Nothing to scale. You try to scale deployment {0} for the same replicas value {1}".format(self.metadata_name, self.spec_replicas))
         if diff_replicas < 0:
             #remove pods
-            for counter in range(diff_replicas):
+            for _ in range(diff_replicas):
                 pod = self.podList.pop(-1)
                 object_space.remove(pod)
         if diff_replicas > 0:
-            create_pods(object_space, diff_replicas)
+            self.create_pods(object_space, diff_replicas)
 
     def check_pod(self, new_pod, object_space):
         for pod in filter(lambda x: isinstance(x, mpod.Pod), object_space):
