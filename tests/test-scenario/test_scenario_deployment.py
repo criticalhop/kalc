e2881a608a9a681bb43c68edf1d6c0dd38258008
Kuznetsov Andrey <andreykyz@gmail.com>
2019-10-09 16:51:18 +0300

    add another multi test function

--- tests/test-scenario/test_scenario_deployment.py
+++ tests/test-scenario/test_scenario_deployment.py
@@ -7,20 +7,22 @@ from guardctl.model.kinds.Service import Service
 from guardctl.model.kinds.PriorityClass import PriorityClass
 from guardctl.model.system.Scheduler import Scheduler
 from guardctl.misc.const import *
-from guardctl.model.search import K8ServiceInterruptSearch, AnyServiceInterrupted
+from guardctl.model.search import K8ServiceInterruptSearch, AnyServiceInterrupted, OptimisticRun
 from guardctl.misc.object_factory import labelFactory
 from poodle import debug_plan
 from poodle.schedule import EmptyPlanError
 from guardctl.model.scenario import Scenario
 import guardctl.model.kinds.Service as mservice
 from tests.test_util import print_objects
+import guardctl.model.kinds.Pod as mpod
 
 #replicas 3 cpu: 100m memory: 500Mi
 DEPLOYMENT_NEW = "./tests/test-scenario/deployment/deployment-new.yaml"
 
 DUMP = "./tests/test-scenario/deployment/dump"
 # cpu = 940m * 2  memory = 2701496Ki + 2701504Ki
-NODES = "./tests/test-scenario/deployment/dump/nodes.yaml"
+NODE1 = "./tests/test-scenario/deployment/dump/node1.yaml"
+NODE2 = "./tests/test-scenario/deployment/dump/node2.yaml"
 # pod cpu = 100m * 7 memory = 500m * 5
 PODS = "./tests/test-scenario/deployment/dump/pods.yaml"
 # the same but one pon in pending TODO may me need to load from cluster
@@ -28,11 +30,15 @@ PODS_PENDING = "./tests/test-scenario/deployment/dump/pods_pending.yaml"
 SERVICES = "./tests/test-scenario/deployment/dump/services.yaml"
 REPLICASETS = "./tests/test-scenario/deployment/dump/replicasets.yaml"
 PRIORITYCLASSES = "./tests/test-scenario/deployment/dump/priorityclass.yaml"
+
+#config file for deployment
 DEPLOYMENT = "./tests/test-scenario/deployment/dump/deployments.yaml"
 
+# @pytest.mark.skip(reason="specific scenario is not selected")
 def test_start_pod():
     k = KubernetesCluster()
-    k.load(open(NODES).read())
+    k.load(open(NODE1).read())
+    k.load(open(NODE2).read())
     k.load(open(PODS).read())
     # k.load(open(PODS_PENDING).read())
     k.load(open(SERVICES).read())
@@ -40,11 +46,79 @@ def test_start_pod():
     k.load(open(PRIORITYCLASSES).read())
     k.load(open(DEPLOYMENT).read())
     k.create_resource(open(DEPLOYMENT_NEW).read())
-
     k._build_state()
-    p = AnyServiceInterrupted(k.state_objects) # self.scheduler.status == STATUS_SCHED["Clean"]
+    class PodStart(K8ServiceInterruptSearch):
+        goal = lambda self: self.goalFoo()
+        
+        def goalFoo(self):
+            for pod in filter(lambda x: isinstance(x, mpod.Pod), k.state_objects):
+                if pod.status != STATUS_POD["Running"]:
+                    return False
+            return True
+
+    p = PodStart(k.state_objects) # self.scheduler.status == STATUS_SCHED["Clean"]
     # print_objects(k.state_objects)
     p.run(timeout=360, sessionName="test_start_pods")
     if not p.plan:
          raise Exception("Could not solve %s" % p.__class__.__name__)
     print(Scenario(p.plan).asyaml())
+    assert "StartPod" in p.plan.__str__() # test for transition from Pending to Running
+    pods = filter(lambda x: isinstance(x, mpod.Pod), k.state_objects)
+    nodes = filter(lambda x: isinstance(x, Node), k.state_objects)
+    for pod in pods:
+        assert pod.atNode in nodes._get_value()
+
+
+class QueueLoadCheck(K8ServiceInterruptSearch):
+    goal = lambda self: self.scheduler.status == STATUS_SCHED["Changed"]
+
+#we have pod with Pendining status in dump we should get it in Running status
+def test_start_pod_from_dump():
+    k = KubernetesCluster()
+    k.load(open(NODE1).read())
+    k.load(open(NODE2).read())
+    k.load(open(PODS_PENDING).read())
+    k.load(open(SERVICES).read())
+    k.load(open(REPLICASETS).read())
+    k.load(open(PRIORITYCLASSES).read())
+    k.load(open(DEPLOYMENT).read())
+    k._build_state()
+    p = QueueLoadCheck(k.state_objects) # self.scheduler.status == STATUS_SCHED["Clean"]
+    # print_objects(k.state_objects)
+    p.run(timeout=360, sessionName="test_start_pod_from_dump")
+    if not p.plan:
+         raise Exception("Could not solve %s" % p.__class__.__name__)
+    print(Scenario(p.plan).asyaml())
+    assert "StartPod" in p.plan.__str__() # test for transition from Pending to Running
+    pods = filter(lambda x: isinstance(x, mpod.Pod), k.state_objects)
+    nodes = filter(lambda x: isinstance(x, Node), k.state_objects)
+    for pod in pods:
+        assert pod.atNode in nodes._get_value() # check each pod than each have atNode
+
+#we have pod with Pendining status in dump we should get it in Running status
+def test_killpod():
+    k = KubernetesCluster()
+    k.load(open(NODE1).read()) # trim resource, run only one Node
+    k.load(open(PODS).read())
+    k.load(open(SERVICES).read())
+    k.load(open(REPLICASETS).read())
+    k.load(open(PRIORITYCLASSES).read())
+    k.load(open(DEPLOYMENT).read())
+
+    k.create_resource(open(NODE1).read())
+
+    k._build_state()
+    p = OptimisticRun(k.state_objects) # TODO check me, i'd like to run exiction test with killpod execution
+    # print_objects(k.state_objects)
+    p.run(timeout=360, sessionName="test_start_pods")
+    if not p.plan:
+         raise Exception("Could not solve %s" % p.__class__.__name__)
+    print(Scenario(p.plan).asyaml())
+    assert "StartPod" in p.plan.__str__() # test for transition from Pending to Running
+    #get pods only in Running state to check atNode value
+    runningPods = filter(lambda z: z.status != STATUS_POD["Running"], (filter(lambda x: isinstance(x, mpod.Pod), k.state_objects)))
+    nodes = filter(lambda x: isinstance(x, Node), k.state_objects)
+    for pod in runningPods:
+        assert pod.atNode in nodes._get_value() # check each pod than each have atNode
+    killingPods = filter(lambda z: z.status != STATUS_POD["Killing"], (filter(lambda x: isinstance(x, mpod.Pod), k.state_objects)))
+    assert len(killingPods) > 0 # test that some pod Killed
