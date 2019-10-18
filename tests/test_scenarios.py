8a086664bb90a9cba5ca9d1ff97011eeeeaefbe7
Makarty <amaximovich@gmail.com>
2019-10-18 12:31:22 +0000

    update test scenario

--- tests/test_scenarios.py
+++ tests/test_scenarios.py
@@ -1,6 +1,6 @@
 from tests.libs_for_tests import *
  
-# test_any_goal
+print("test_any_goal")
 
 # @pytest.mark.skip(reason="temporary skip")
 def test_AnyGoal_wo_cli():
@@ -23,16 +23,16 @@ def test_has_deployment_creates_daemonset__pods_evicted_pods_pending_cli_direct(
 def test_has_deployment_creates_daemonset__pods_evicted_pods_pending_cli_invoke():
     run_cli_invoke(DUMP_S1_HIGH_PRIORITY_S2_ZERO_PRIORITY,CHANGE_DAEMONSET_HIGH)
  
- ## test_creates_deployment_but_insufficient_resource__pods_pending
+print("test_creates_deployment_but_insufficient_resource__pods_pending")
 
-# def test_creates_deployment_but_insufficient_resource__pods_pending_wo_cli():
-#     run_wo_cli(DUMP_S1_HIGH_PRIORITY_S2_ZERO_PRIORITY,CHANGE_DAEMONSET_HIGH)
-# # @pytest.mark.skip(reason="temporary skip")
-# def test_creates_deployment_but_insufficient_resource__pods_pending_cli_direct():
-#     run_cli_directly(DUMP_S1_HIGH_PRIORITY_S2_ZERO_PRIORITY,CHANGE_DAEMONSET_HIGH)
-# # @pytest.mark.skip(reason="temporary skip")
-# def test_creates_deployment_but_insufficient_resource__pods_pending_cli_invoke():
-#     run_cli_invoke(DUMP_S1_HIGH_PRIORITY_S2_ZERO_PRIORITY,CHANGE_DAEMONSET_ZERO)
+def test_creates_deployment_but_insufficient_resource__pods_pending_wo_cli():
+    run_wo_cli(DUMP_S1_HIGH_PRIORITY_S2_ZERO_PRIORITY,CHANGE_DEPLOYMENT_ZERO)
+# @pytest.mark.skip(reason="temporary skip")
+def test_creates_deployment_but_insufficient_resource__pods_pending_cli_direct():
+    run_cli_directly(DUMP_S1_HIGH_PRIORITY_S2_ZERO_PRIORITY,CHANGE_DEPLOYMENT_ZERO)
+# @pytest.mark.skip(reason="temporary skip")
+def test_creates_deployment_but_insufficient_resource__pods_pending_cli_invoke():
+    run_cli_invoke(DUMP_S1_HIGH_PRIORITY_S2_ZERO_PRIORITY,CHANGE_DEPLOYMENT_ZERO)
  
 ## test_creates_service_and_deployment_insufficient_resource__service_outage
 
