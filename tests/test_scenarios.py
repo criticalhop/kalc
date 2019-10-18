a6ee9bfd776fa5749b6f4a65e165fb34be0edb39
Makarty <amaximovich@gmail.com>
2019-10-18 12:29:06 +0000

    extra test remove

--- tests/test_scenarios.py
+++ tests/test_scenarios.py
@@ -12,7 +12,7 @@ def test_AnyGoal_cli_direct():
 def test_AnyGoal_cli_invoke():
     run_cli_invoke(DUMP_S1_HIGH_PRIORITY_S2_ZERO_PRIORITY,CHANGE_DEPLOYMENT_HIGH)
 
-## test_has_deployment_creates_daemonset__pods_evicted_pods_pending
+print("test_has_deployment_creates_daemonset__pods_evicted_pods_pending")
 
 def test_has_deployment_creates_daemonset__pods_evicted_pods_pending_wo_cli():
     run_wo_cli(DUMP_S1_HIGH_PRIORITY_S2_ZERO_PRIORITY,CHANGE_DAEMONSET_HIGH)
@@ -25,14 +25,14 @@ def test_has_deployment_creates_daemonset__pods_evicted_pods_pending_cli_invoke(
  
  ## test_creates_deployment_but_insufficient_resource__pods_pending
 
-def test_creates_deployment_but_insufficient_resource__pods_pending_wo_cli():
-    run_wo_cli(DUMP_S1_HIGH_PRIORITY_S2_ZERO_PRIORITY,CHANGE_DAEMONSET_HIGH)
-# @pytest.mark.skip(reason="temporary skip")
-def test_creates_deployment_but_insufficient_resource__pods_pending_cli_direct():
-    run_cli_directly(DUMP_S1_HIGH_PRIORITY_S2_ZERO_PRIORITY,CHANGE_DAEMONSET_HIGH)
-# @pytest.mark.skip(reason="temporary skip")
-def test_creates_deployment_but_insufficient_resource__pods_pending_cli_invoke():
-    run_cli_invoke(DUMP_S1_HIGH_PRIORITY_S2_ZERO_PRIORITY,CHANGE_DAEMONSET_ZERO)
+# def test_creates_deployment_but_insufficient_resource__pods_pending_wo_cli():
+#     run_wo_cli(DUMP_S1_HIGH_PRIORITY_S2_ZERO_PRIORITY,CHANGE_DAEMONSET_HIGH)
+# # @pytest.mark.skip(reason="temporary skip")
+# def test_creates_deployment_but_insufficient_resource__pods_pending_cli_direct():
+#     run_cli_directly(DUMP_S1_HIGH_PRIORITY_S2_ZERO_PRIORITY,CHANGE_DAEMONSET_HIGH)
+# # @pytest.mark.skip(reason="temporary skip")
+# def test_creates_deployment_but_insufficient_resource__pods_pending_cli_invoke():
+#     run_cli_invoke(DUMP_S1_HIGH_PRIORITY_S2_ZERO_PRIORITY,CHANGE_DAEMONSET_ZERO)
  
 ## test_creates_service_and_deployment_insufficient_resource__service_outage
 
