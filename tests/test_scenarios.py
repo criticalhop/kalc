7646fa7d7e793f6b9c109ecf6a19c4f1a68ba64f
Makarty <amaximovich@gmail.com>
2019-10-18 14:40:31 +0000

    remoe direct test

--- tests/test_scenarios.py
+++ tests/test_scenarios.py
@@ -5,7 +5,7 @@ print("test_any_goal")
 # @pytest.mark.skip(reason="temporary skip")
 def test_AnyGoal_wo_cli():
     run_wo_cli(DUMP_S1_HIGH_PRIORITY_S2_ZERO_PRIORITY,CHANGE_DEPLOYMENT_HIGH)
-# @pytest.mark.skip(reason="temporary skip")
+@pytest.mark.skip(reason="temporary skip")
 def test_AnyGoal_cli_direct():
     run_cli_directly(DUMP_S1_HIGH_PRIORITY_S2_ZERO_PRIORITY,CHANGE_DEPLOYMENT_HIGH)
 # @pytest.mark.skip(reason="temporary skip")
@@ -16,7 +16,7 @@ print("test_has_deployment_creates_daemonset__pods_evicted_pods_pending")
 
 def test_has_deployment_creates_daemonset__pods_evicted_pods_pending_wo_cli():
     run_wo_cli(DUMP_S1_HIGH_PRIORITY_S2_ZERO_PRIORITY,CHANGE_DAEMONSET_HIGH)
-# @pytest.mark.skip(reason="temporary skip")
+@pytest.mark.skip(reason="temporary skip")
 def test_has_deployment_creates_daemonset__pods_evicted_pods_pending_cli_direct():
     run_cli_directly(DUMP_S1_HIGH_PRIORITY_S2_ZERO_PRIORITY,CHANGE_DAEMONSET_HIGH)
 # @pytest.mark.skip(reason="temporary skip")
@@ -27,7 +27,7 @@ print("test_creates_deployment_but_insufficient_resource__pods_pending")
 
 def test_creates_deployment_but_insufficient_resource__pods_pending_wo_cli():
     run_wo_cli(DUMP_S1_HIGH_PRIORITY_S2_ZERO_PRIORITY,CHANGE_DEPLOYMENT_ZERO)
-# @pytest.mark.skip(reason="temporary skip")
+@pytest.mark.skip(reason="temporary skip")
 def test_creates_deployment_but_insufficient_resource__pods_pending_cli_direct():
     run_cli_directly(DUMP_S1_HIGH_PRIORITY_S2_ZERO_PRIORITY,CHANGE_DEPLOYMENT_ZERO)
 # @pytest.mark.skip(reason="temporary skip")
