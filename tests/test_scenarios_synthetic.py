90fb71bdc7c550a52862067c8b118d8bb84ce13f
Andrew Gree <andrew@criticalhop.com>
2019-10-20 02:42:59 +0000

    added synthetic tests

---
+++ tests/test_scenarios_synthetic.py
@@ -0,0 +1,341 @@
+from tests.test_util import print_objects
+from guardctl.model.search import AnyGoal 
+from guardctl.model.system.Scheduler import Scheduler
+from guardctl.model.kinds.Service import Service
+from guardctl.model.kinds.Node import Node
+from guardctl.model.kinds.Pod import Pod
+from guardctl.model.kinds.Deployment import Deployment
+from guardctl.model.kinds.DaemonSet import DaemonSet
+from guardctl.model.kinds.PriorityClass import PriorityClass
+from guardctl.model.kubernetes import KubernetesCluster
+from guardctl.misc.const import *
+
+def build_running_pod(podName, cpuRequest, memRequest, atNode):
+    pod_running_1 = Pod()
+    pod_running_1.metadata_name = "pod"+str(podName)
+    pod_running_1.cpuRequest = 2
+    pod_running_1.memRequest = 2
+    pod_running_1.atNode = atNode
+    pod_running_1.status = STATUS_POD["Running"]
+    return pod_running_1
+
+def build_pending_pod(podName, cpuRequest, memRequest, toNode):
+    p = build_running_pod(podName, cpuRequest, memRequest, Node.NODE_NULL)
+    p.status = STATUS_POD["Pending"]
+    p.toNode = toNode 
+    return p
+
+def test_run_pods_no_eviction():
+    # TODO: extract final status for loader unit tests from here
+    # Initialize scheduler, globalvar
+    k = KubernetesCluster()
+    scheduler = next(filter(lambda x: isinstance(x, Scheduler), k.state_objects))
+    # initial node state
+    n = Node()
+    n.cpuCapacity = 5
+    n.memCapacity = 5
+
+    n.currentFormalCpuConsumption = 0
+    n.currentFormalMemConsumption = 0
+
+    # priority - as needed
+    pc = PriorityClass()
+    pc.priority = 10
+    pc.metadata_name = "high-prio-test"
+
+    # Pedning pod
+    pod_pending_1 = build_pending_pod(3,2,2,n)
+    pod_pending_1.priorityClass = pc
+
+    ## Add pod to scheduler queue
+    scheduler.podQueue.add(pod_pending_1)
+    scheduler.queueLength += 1
+    scheduler.status = STATUS_SCHED["Changed"]
+
+    # Create a "holding" controller - optional
+    ds = DaemonSet()
+    ds.podList.add(pod_pending_1)
+
+    k.state_objects.extend([n, pc, pod_pending_1, ds])
+    # print_objects(k.state_objects)
+    class NewGOal(AnyGoal):
+        goal = lambda self: pod_pending_1.status == STATUS_POD["Running"]
+    p = NewGOal(k.state_objects)
+    p.run(timeout=50)
+    # TODO: fix strange behaviour -->>
+    # assert "StartPod" in "\n".join([x() for x in p.plan])
+    assert "StartPod" in "\n".join([repr(x) for x in p.plan])
+    # for a in p.plan:
+    #     print(a) 
+
+
+def test_run_pods_with_eviction():
+    # Initialize scheduler, globalvar
+    k = KubernetesCluster()
+    scheduler = next(filter(lambda x: isinstance(x, Scheduler), k.state_objects))
+    # initial node state
+    n = Node()
+    n.cpuCapacity = 5
+    n.memCapacity = 5
+
+    # Create running pods
+    pod_running_1 = build_running_pod(1,2,2,n)
+    pod_running_2 = build_running_pod(2,2,2,n)
+
+    ## Set consumptoin as expected
+    n.currentFormalCpuConsumption = 4
+    n.currentFormalMemConsumption = 4
+
+    # priority for pod-to-evict
+    pc = PriorityClass()
+    pc.priority = 10
+    pc.metadata_name = "high-prio-test"
+
+    # Pending pod
+    pod_pending_1 = build_pending_pod(3,2,2,n)
+    pod_pending_1.priorityClass = pc # high prio will evict!
+
+    ## Add pod to scheduler queue
+    scheduler.podQueue.add(pod_pending_1)
+    scheduler.queueLength += 1
+    scheduler.status = STATUS_SCHED["Changed"]
+
+    k.state_objects.extend([n, pc, pod_running_1, pod_running_2, pod_pending_1])
+    # print_objects(k.state_objects)
+    class NewGOal(AnyGoal):
+        goal = lambda self: pod_pending_1.status == STATUS_POD["Running"]
+    p = NewGOal(k.state_objects)
+    p.run(timeout=50)
+    assert "StartPod" in "\n".join([repr(x) for x in p.plan])
+    assert "Evict" in "\n".join([repr(x) for x in p.plan])
+    # for a in p.plan:
+        # print(a)
+
+
+def test_synthetic_service_outage():
+    # Initialize scheduler, globalvar
+    k = KubernetesCluster()
+    scheduler = next(filter(lambda x: isinstance(x, Scheduler), k.state_objects))
+    # initial node state
+    n = Node()
+    n.cpuCapacity = 5
+    n.memCapacity = 5
+
+    # Create running pods
+    pod_running_1 = build_running_pod(1,2,2,n)
+    pod_running_2 = build_running_pod(2,2,2,n)
+
+    ## Set consumptoin as expected
+    n.currentFormalCpuConsumption = 4
+    n.currentFormalMemConsumption = 4
+
+    # priority for pod-to-evict
+    pc = PriorityClass()
+    pc.priority = 10
+    pc.metadata_name = "high-prio-test"
+
+    # Service to detecte eviction
+    s = Service()
+    s.metadata_name = "test-service"
+    s.amountOfActivePods = 1
+    s.status = STATUS_SERV["Started"]
+
+    # our service has only one pod so it can detect outage
+    #  (we can't evict all pods here with one)
+    pod_running_1.targetService = s
+
+    # Pending pod
+    pod_pending_1 = build_pending_pod(3,2,2,n)
+    pod_pending_1.priorityClass = pc # high prio will evict!
+
+    ## Add pod to scheduler queue
+    scheduler.podQueue.add(pod_pending_1)
+    scheduler.queueLength += 1
+    scheduler.status = STATUS_SCHED["Changed"]
+
+    k.state_objects.extend([n, pc, pod_running_1, pod_running_2, pod_pending_1])
+    # print_objects(k.state_objects)
+    class NewGOal(AnyGoal):
+        pass
+        # goal = lambda self: pod_pending_1.status == STATUS_POD["Running"]
+    p = NewGOal(k.state_objects)
+    p.run(timeout=50)
+    assert "StartPod" in "\n".join([repr(x) for x in p.plan])
+    assert "Evict" in "\n".join([repr(x) for x in p.plan])
+    assert "MarkServiceOutageEvent" in "\n".join([repr(x) for x in p.plan])
+    # for a in p.plan:
+        # print(a)
+
+def construct_multi_pods_eviction_problem():
+    # Initialize scheduler, globalvar
+    k = KubernetesCluster()
+    scheduler = next(filter(lambda x: isinstance(x, Scheduler), k.state_objects))
+    # initial node state
+    n = Node()
+    n.cpuCapacity = 5
+    n.memCapacity = 5
+
+    # Create running pods
+    pod_running_1 = build_running_pod(1,2,2,n)
+    pod_running_2 = build_running_pod(2,2,2,n)
+
+    ## Set consumptoin as expected
+    n.currentFormalCpuConsumption = 4
+    n.currentFormalMemConsumption = 4
+
+    # priority for pod-to-evict
+    pc = PriorityClass()
+    pc.priority = 10
+    pc.metadata_name = "high-prio-test"
+
+    # Service to detecte eviction
+    s = Service()
+    s.metadata_name = "test-service"
+    s.amountOfActivePods = 2
+    s.status = STATUS_SERV["Started"]
+
+    # our service has only one pod so it can detect outage
+    #  (we can't evict all pods here with one)
+    # TODO: no outage detected if res is not 4
+    pod_running_1.targetService = s
+    pod_running_2.targetService = s
+
+    # Pending pod
+    pod_pending_1 = build_pending_pod(3,4,4,n)
+    pod_pending_1.priorityClass = pc # high prio will evict!
+
+    ## Add pod to scheduler queue
+    scheduler.podQueue.add(pod_pending_1)
+    scheduler.queueLength += 1
+    scheduler.status = STATUS_SCHED["Changed"]
+
+    k.state_objects.extend([n, pc, pod_running_1, pod_running_2, pod_pending_1])
+    # print_objects(k.state_objects)
+    return k
+
+def test_synthetic_service_outage_multi():
+    "Multiple pods are evicted from one service to cause outage"
+    k = construct_multi_pods_eviction_problem()
+    class NewGOal(AnyGoal):
+        pass
+        # goal = lambda self: pod_pending_1.status == STATUS_POD["Running"]
+    p = NewGOal(k.state_objects)
+    p.run(timeout=50)
+    assert "StartPod" in "\n".join([repr(x) for x in p.plan])
+    assert "Evict" in "\n".join([repr(x) for x in p.plan])
+    assert "MarkServiceOutageEvent" in "\n".join([repr(x) for x in p.plan])
+    # for a in p.plan:
+        # print(a)
+
+
+def test_synthetic_service_NO_outage_multi():
+    "No outage is caused by evicting only one pod of a multi-pod service"
+    # Initialize scheduler, globalvar
+    k = KubernetesCluster()
+    scheduler = next(filter(lambda x: isinstance(x, Scheduler), k.state_objects))
+    # initial node state
+    n = Node()
+    n.cpuCapacity = 5
+    n.memCapacity = 5
+
+    # Create running pods
+    pod_running_1 = build_running_pod(1,2,2,n)
+    pod_running_2 = build_running_pod(2,2,2,n)
+
+    ## Set consumptoin as expected
+    n.currentFormalCpuConsumption = 4
+    n.currentFormalMemConsumption = 4
+
+    # priority for pod-to-evict
+    pc = PriorityClass()
+    pc.priority = 10
+    pc.metadata_name = "high-prio-test"
+
+    # Service to detecte eviction
+    s = Service()
+    s.metadata_name = "test-service"
+    s.amountOfActivePods = 2
+    s.status = STATUS_SERV["Started"]
+
+    # our service has only one pod so it can detect outage
+    #  (we can't evict all pods here with one)
+    # TODO: no outage detected if res is not 4
+    pod_running_1.targetService = s
+    pod_running_2.targetService = s
+
+    # Pending pod
+    pod_pending_1 = build_pending_pod(3,2,2,n)
+    pod_pending_1.priorityClass = pc # high prio will evict!
+
+    ## Add pod to scheduler queue
+    scheduler.podQueue.add(pod_pending_1)
+    scheduler.queueLength += 1
+    scheduler.status = STATUS_SCHED["Changed"]
+
+    k.state_objects.extend([n, pc, pod_running_1, pod_running_2, pod_pending_1])
+    # print_objects(k.state_objects)
+    class NewGOal(AnyGoal):
+        pass
+        # goal = lambda self: pod_pending_1.status == STATUS_POD["Running"]
+    p = NewGOal(k.state_objects)
+    p.run(timeout=50)
+    if p.plan:
+        print("ERROR!!!")
+        for a in p.plan:
+            print(a)
+        raise Exception("Plan must be empty in this case")
+
+
+def test_has_deployment_creates_daemonset__pods_evicted_pods_pending_wo_cli_synthetic():
+    "No outage is caused by evicting only one pod of a multi-pod service"
+    # Initialize scheduler, globalvar
+    k = KubernetesCluster()
+    scheduler = next(filter(lambda x: isinstance(x, Scheduler), k.state_objects))
+    # initial node state
+    n = Node()
+    n.cpuCapacity = 5
+    n.memCapacity = 5
+
+    # Create running pods
+    pod_running_1 = build_running_pod(1,2,2,n)
+    pod_running_2 = build_running_pod(2,2,2,n)
+
+    ## Set consumptoin as expected
+    n.currentFormalCpuConsumption = 4
+    n.currentFormalMemConsumption = 4
+
+    # priority for pod-to-evict
+    pc = PriorityClass()
+    pc.priority = 10
+    pc.metadata_name = "high-prio-test"
+
+    # Service to detecte eviction
+    s = Service()
+    s.metadata_name = "test-service"
+    s.amountOfActivePods = 2
+    s.status = STATUS_SERV["Started"]
+
+    # our service has only one pod so it can detect outage
+    #  (we can't evict all pods here with one)
+    # TODO: no outage detected if res is not 4
+    pod_running_1.targetService = s
+    pod_running_2.targetService = s
+
+    # Pending pod
+    pod_pending_1 = build_pending_pod(3,2,2,n)
+    pod_pending_1.priorityClass = pc # high prio will evict!
+
+    ## Add pod to scheduler queue
+    scheduler.podQueue.add(pod_pending_1)
+    scheduler.queueLength += 1
+    scheduler.status = STATUS_SCHED["Changed"]
+
+    k.state_objects.extend([n, pc, pod_running_1, pod_running_2, pod_pending_1])
+    # print_objects(k.state_objects)
+    class NewGOal(AnyGoal):
+        pass
+        # goal = lambda self: pod_pending_1.status == STATUS_POD["Running"]
+    p = NewGOal(k.state_objects)
+    p.run(timeout=50)
+    assert not p.plan, "Plan must be empty in this case"
+
