e32ec945ae8293b9eb5e9355cc6ba50b6cba035a
Makarty <amaximovich@gmail.com>
2019-11-08 12:29:47 +0000

    added statuses to testsstats

--- tests/testsstats.py
+++ tests/testsstats.py
@@ -28,15 +28,19 @@ test_cases=[["test_36","100","100"],\
                 ["test_40","100","100"],\
                 ["test_40","100","20"]]#test_name,lin_count,weight,
 
-command_to_get_git_commit = "git rev-parse --short HEAD; git branch | grep \* | cut -d ' ' -f2 > " + file_for_commit
+command_to_get_git_commit = "git rev-parse --short HEAD >"+ file_for_commit + "; git branch | grep \* | cut -d ' ' -f2 >> " + file_for_commit
 os.system(command_to_get_git_commit)
-
+commit_item = ['']
 with open(file_for_commit) as f_commit:
-    commit_item = f_commit.read().replace('\n', '')
+    commit_item = f_commit.read().splitlines()
+
+commit_item_inline= ''
+for line in commit_item:
+     commit_item_inline = commit_item_inline + line + ' '
 
 f_report = open(file_for_report,"a+")
-f_report.write('tested commit: '+ commit_item)
-print(commit_item)
+f_report.write('tested commit: '+ commit_item_inline +"\r\n")
+print(commit_item_inline)
 
 for i in test_cases:
     test_name = i[0]
@@ -47,13 +51,17 @@ for i in test_cases:
 
     template = "POODLE_LIN_COUNT={} POODLE_ASTAR_WEIGHT={} PYTHON=pypy POODLE_SOLVER_URL=http://localhost:{} tox -e poodledev -- -s ./tests/test_scenarios_synthetic.py::{} > {}"
     bashCommand = template.format(lin_count,weight,port, test_name,log_name)
-    print('test_name, '+ test_name +', lin_count, '+ lin_count + ', weight ,'+ weight)
-    os.system(bashCommand)
+    # os.system(bashCommand)
+    duration2 = ['0']
+    status = "FAILED"
     with open('./'+ log_name) as f:
         lines = f.read().splitlines()
-    for a in lines:    
-        if "s call     tests/test_scenarios_synthetic.py::"+test_name in a:
-            duration2 = re.findall(r'(\d*)\.', a)
-            stats_item = 'test_name, '+ test_name +', lin_count, '+ lin_count + ', weight ,'+ weight+', duration, '+ duration2[0]
-            f_report.write("" + stats_item + "\r\n")
-            print(stats_item)
+    for a in lines:
+        if "PASSED" in a:
+            for a in lines:    
+                if "s call     tests/test_scenarios_synthetic.py::"+test_name in a:
+                    duration2 = re.findall(r'(\d*)\.', a)
+                    status = "PASSED"
+    stats_item = 'test_name, '+ test_name +', lin_count, '+ lin_count + ', weight ,' + weight + ', status,' + status + ', duration, '+ duration2[0]
+    f_report.write("" + stats_item + "\r\n")
+    print(stats_item)
