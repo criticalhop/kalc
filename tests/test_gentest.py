b1b455cf16f1ec462babac83361567ef8030fa8d
Kuznetsov Andrey <andreykyz@gmail.com>
2019-11-11 14:38:56 +0300

    well generator

--- tests/test_gentest.py
+++ tests/test_gentest.py
@@ -17,8 +17,8 @@ from guardctl.misc.object_factory import labelFactory
 from click.testing import CliRunner
 from guardctl.model.scenario import Scenario
 from poodle import planned
-from tests.libs_for_tests import convert_space_to_yaml,print_objects_from_yaml,print_plan,load_yaml, print_objects_compare, checks_assert_conditions, reload_cluster_from_yaml
-from tests.test_scenarios_synthetic import prepare_test_29_many_pods_not_enough_capacity_for_service, build_running_pod_with_d
+from tests.libs_for_tests import convert_space_to_yaml,print_objects_from_yaml,print_plan,load_yaml, print_objects_compare, checks_assert_conditions, reload_cluster_from_yaml, checks_assert_conditions_in_one_mode
+from tests.test_scenarios_synthetic import build_running_pod_with_d
 
 DEBUG_MODE = 1
 
@@ -74,38 +74,32 @@ def prepare_test(nodes_amount,node_capacity,pod_amount,pod_w_service_amount):
     pc = PriorityClass()
     pc.priority = 10
     pc.metadata_name = "high-prio-test"
-
     
     k.state_objects.extend(nodes)
     k.state_objects.extend(pods)
     k.state_objects.extend([pc, s, s2 ])
     create_objects = []
-    k2 = reload_cluster_from_yaml(k,create_objects)
     k._build_state()
-    class NewGoal_k1(CheckNodeOutage):
-        pass
-    p = NewGoal_k1(k.state_objects)
-    class NewGoal_k2(CheckNodeOutage):
-        pass
-    p2 = NewGoal_k2(k2.state_objects)
-    assert_conditions = ["MarkServiceOutageEvent",\
-                        "Mark_node_outage_event"]
-    not_assert_conditions = []
-    return k, k2, p , p2
+    genClass = type("gen_test_{0}_{1}".format(node_capacity, pod_amount),(CheckNodeOutage))
+    p = genClass(k.state_objects)
+    return k, p
 
 def test_gen_1():
-    for node in range(24,24):
-        for cap in range(10,20):
+    assert_brake = False
+    for node_cap in range(24,50):
+        for pod_amount in range(10,50):
             try:
-                k, k2,p ,p2 = prepare_test_29_many_pods_not_enough_capacity_for_service(1,node,cap,0,0,1)
+                k, p = prepare_test(1,node_cap,pod_amount,1)
             except Exception as e:
-                print("prepare break node ", node, " cap " ,cap, "exception is \n",e)
+                print("prepare break node cap ", node_cap, " pod amount " ,pod_amount, "exception is \n",e)
                 break
             assert_conditions = ["MarkServiceOutageEvent", "Mark_node_outage_event"]
             not_assert_conditions = []
             try:
-                assert_brake = checks_assert_conditions(k,k2,p,p2,assert_conditions,not_assert_conditions,DEBUG_MODE)
+                assert_brake = checks_assert_conditions_in_one_mode(k,p,assert_conditions,not_assert_conditions, "test_{0}_{1}".format(node_cap, pod_amount), 0)
             except Exception as e:
-                print("check break node ", node, " cap " ,cap, "exception is \n",e)
+                print("check break node ", node_cap, " cap " ,pod_amount, "exception is \n",e)
                 break
-    assert True
+            if assert_brake:
+                break
+    assert False
