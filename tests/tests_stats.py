e8de2de3359344ea7e5bc6b93c08b31751c3c391
Makarty <amaximovich@gmail.com>
2019-11-22 22:49:38 +0000

    cleaned up tests scripts

--- tests/tests_stats.py
+++ tests/tests_stats.py
@@ -3,7 +3,7 @@ import os
 import re
 
 
-comment = "costrange200"
+comment = "cost250-500-poodle29e0e61"
 file_for_commit = "./log-current-commit" + "-" + comment
 file_for_list_of_reports = "./log_list_of_reports"
 
@@ -95,6 +95,8 @@ test_cases = [
 lin_count = "50"
 weight = "1"
 test_file = "test_synthetic_hypothesis_run.py"
+domain_high_cost = 500
+domain_middle_cost = 250
 
 command_to_get_git_commit = "git rev-parse --short HEAD >"+ file_for_commit + "; git branch | grep \* | cut -d ' ' -f2 >> " + file_for_commit
 os.system(command_to_get_git_commit)
@@ -121,6 +123,6 @@ for i in test_cases:
     f_report_list.write(str(log_params) +"\r\n")
     log_name = "log-" + log_id
     port = randrange(10000, 10101, 1) 
-    template = "OUT_NAME={} POODLE_LIN_COUNT={} POODLE_ASTAR_WEIGHT={} PYTHON=pypy POODLE_SOLVER_URL=http://localhost:{} tox -e poodledev -- -s ./tests/{}::{} > {}"
-    bashCommand = template.format(log_name, lin_count, weight, port, test_file, test_name, log_name)
+    template = "DOMAIN_HIGH_COST={} DOMAIN_MIDDLE_COST={} OUT_NAME={} POODLE_LIN_COUNT={} POODLE_ASTAR_WEIGHT={} PYTHON=pypy POODLE_SOLVER_URL=http://localhost:{} tox -e poodledev -- -s ./tests/{}::{} > {}"
+    bashCommand = template.format(domain_high_cost, domain_middle_cost, log_name, lin_count, weight, port, test_file, test_name, log_name)
     os.system(bashCommand)
