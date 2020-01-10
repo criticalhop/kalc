e7a9ddc670999043007050452b9703da9d454fe9
Makarty <amaximovich@gmail.com>
2020-01-10 10:22:06 -0800

    added actions for node drain functionality

--- kalc/model/system/globals.py
+++ kalc/model/system/globals.py
@@ -1,6 +1,7 @@
 from poodle import Object
 from kalc.model.system.primitives import Type, Status
 import kalc.model.kinds.Deployment as mdeployment
+import kalc.model.kinds.Node as mnode
 from typing import Set
 
 class GlobalVar(Object):
@@ -30,7 +31,9 @@ class GlobalVar(Object):
     maxNumberOfPodsOnSameNodeForDeployment: int
     DeploymentsWithAntiaffinity: Set["mdeployment.Deployment"]
     DeploymentsWithAntiaffinity_length: int
-    
+    NodesDrained: Set["mnode.Node"]
+    NodesDrained_lenght: int
+    target_NodesDrained_length_reached: bool
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -56,4 +59,7 @@ class GlobalVar(Object):
         self.target_amountOfPodsWithAntiaffinity = 0
         self.deploymentsWithAntiaffinityBalanced = False
         self.maxNumberOfPodsOnSameNodeForDeployment = 0
+        self.NodesDrained_lenght = 0
+        self.target_NodesDrained_length_reached = False
+        
     def __str__(self): return str(self._get_value())
