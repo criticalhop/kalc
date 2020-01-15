03e80e637ed52062fd9d2cf521431f7ae7e91352
Kuznetsov Andrey <andreykyz@gmail.com>
2020-01-15 16:49:47 +0300

    add faultTolerance mentric

--- kalc/misc/metrics.py
+++ kalc/misc/metrics.py
@@ -1,27 +1,48 @@
-from kalc.model.system.base import ModularKind
-from kalc.model.system.Controller import Controller
-from kalc.model.system.base import HasLimitsRequests
-from kalc.model.kinds.PriorityClass import PriorityClass, zeroPriorityClass
-from kalc.model.system.Scheduler import Scheduler
-import kalc.model.kinds.Pod as mpod
-import kalc.model.system.globals as mGlobalVar
-from kalc.model.kinds.ReplicaSet import ReplicaSet
-from kalc.model.system.primitives import Status, Label
-from kalc.misc.const import STATUS_POD, STATUS_SCHED, StatusDeployment
-import kalc.model.kinds.Node as mnode
-import kalc.model.kinds.Deployment as mdeployment
-from poodle import *
-from typing import Set
+from kalc.model.kinds.Pod import 
+from kalc.model.kinds.Node import
+from kalc.model.kinds.Deployment import Deployment
 from logzero import logger
-import kalc.misc.util as util
-import random
-import yaml, copy, jsonpatch, difflib
+import math
 
-def calculate_maxNumberOfPodsOnSameNode_metrics(self, object_space):
-    deployments = filter(lambda x: isinstance(x, mdeployment.Deployment), object_space)
-    pods = filter(lambda x: isinstance(x, mpod.Pod), object_space)
-    nodes = filter(lambda x: isinstance(x,mnode.Node), object_space)
-    globalVar = next(filter(lambda x: isinstance(x, mGlobalVar.GlobalVar), object_space))
+class Metric():
+    maxNumberOfPodsOnSameNodeForDeployment: int
+    meanPercentageOnTheSameNode: int
+
+    def __init__(self, object_space):
+        self.deployments = filter(lambda x: isinstance(x, mdeployment.Deployment), object_space)
+        self.pods = filter(lambda x: isinstance(x, mpod.Pod), object_space)
+        self.nodes = filter(lambda x: isinstance(x,mnode.Node), object_space)
+        self.globalVar = next(filter(lambda x: isinstance(x, mGlobalVar.GlobalVar), object_space))
+        self.setUnusedRes()
+
+    def setUnusedRes(self):
+        self.cpuUsed = 0
+        self.cpuTotal = 0
+        self.memUsed = 0 
+        self.memTotal = 0
+        for node in self.nodes:
+            self.cpuTotal += node.cpuCapacity
+            self.memTotal += node.memCapacity
+        for pod in self.pods:
+            self.cpuUsed += podcurrentRealCpuConsumption
+            self.memUsed += podcurrentRealCpuConsumption
+        self.memFree = self.cpuTotal - self.cpuUsed
+        self.cpuFree = self.cpuTotal - self.cpuUsed 
+
+    def faultTolerance(self):
+        self.faultTolerance = {}
+        faultTolerance = 0
+        nodes = {}
+        for deployment in self.deployments:
+            for pod in deployment.pod_list:
+                nodes[id(pod.atNode)] = nodes.get(id(pod.atNode), 0.0) + 1.0
+            for node in nodes:
+                faultToleranceSquare += (float(node) / float(len(deployment.pod_list))) ** 2
+                faultToleranceGeom += float(node) / float(len(deployment.pod_list))
+            self.faultToleranceSquare = math.sqrt(faultTolerance)
+            self.faultToleranceGeom = math.pow(faultToleranceGeom, len(node))
+
+
     deploymentController_max_node_amount_of_pods_list = []
     for deploymentController in deployments:
         node_amount_of_pods_list = []
