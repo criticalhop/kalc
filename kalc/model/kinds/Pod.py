68f26c85e85f888aa9230e50d01d65868a3baeb8
Makarty <amaximovich@gmail.com>
2020-01-17 12:52:03 -0800

    added calculation of allocatedPodList_length at load

--- kalc/model/kinds/Pod.py
+++ kalc/model/kinds/Pod.py
@@ -152,6 +152,7 @@ class Pod(ModularKind, HasLabel, HasLimitsRequests):
             if str(node.metadata_name) == str(self.spec_nodeName):
                 self.atNode = node
                 node.allocatedPodList.add(self)
+                node.allocatedPodList_length += 1
                 node.directedPodList.add(self)
                 node.amountOfActivePods += 1
                 assert getint(node.amountOfActivePods) < POODLE_MAXLIN, "Pods amount exceeded max %s > %s" % (getint(node.amountOfActivePods), POODLE_MAXLIN) 
