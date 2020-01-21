81855fd32bff0f82f16e162d67c9174b0bdef313
Makarty <amaximovich@gmail.com>
2020-01-21 13:09:47 -0800

    dded restriction for singleton pod

--- kalc/model/kubeactions.py
+++ kalc/model/kubeactions.py
@@ -405,8 +405,10 @@ class KubernetesModel(ProblemTemplate):
         nodeFrom: "Node",
         nodeTo: "Node",
         scheduler: "Scheduler",
-        globalVar: GlobalVar
-        ):
+        globalVar: GlobalVar,
+        deployment: Deployment):
+        if pod in deployment.podList:
+            assert deployment.amountOfActivePods > 1
         # assert globalVar.block_policy_calculated == False
         assert pod.atNode == nodeFrom
         assert pod.status == STATUS_POD["Running"]
